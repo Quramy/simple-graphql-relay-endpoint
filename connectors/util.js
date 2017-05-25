5679b85de2a68bcb2cc97e75994ecfaf2719be01
y-kurami <yosuke.kurami@gmail.com>
2017-05-25 14:19:37 +0900

    Chore

--- connectors/util.js
+++ connectors/util.js
@@ -1,5 +1,3 @@
 export function converter(model) {
-  const ret = Object.assign({}, model);
-  ret.id = ret._id;
-  return ret;
+  return { ...model, id: model._id };
 }
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import graphqlHttp from 'express-graphql';
+import graphqlHTTP from 'express-graphql';
 import { schema } from './schema/schema';
 
 const GRAPHQL_PORT = 4200;
 
-const graphQLServer = express();
-graphQLServer.use((req, res, next) => {
+const server = express();
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -18,7 +18,5 @@ graphQLServer.use((req, res, next) => {
   }
 });
 
-graphQLServer.use('/graphql', graphqlHttp({ schema, pretty: true }));
-graphQLServer.listen(GRAPHQL_PORT, () => {
-  console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`);
-});
+server.use('/graphql', graphqlHTTP({ schema, pretty: true }));
+server.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
